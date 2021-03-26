9cd878a3c2b270c94fb6d1c2fec469f29370cf34
wojciech.strzalinski <wojciech.strzaliski@apsenergia.pl>
2021-03-26 15:13:48 +0100

    Added alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
 	"""class to manage game"""
@@ -21,6 +22,10 @@ class AlienInvasion:
 		
 		self.ship = Ship(self);
 		self.bullets = pygame.sprite.Group()
+		self.aliens = pygame.sprite.Group()
+
+		self._create_fleet()
+
 		
 	
 	def run_game(self):
@@ -89,9 +94,13 @@ class AlienInvasion:
 		self.ship.blitme()
 		for bullet in self.bullets.sprites():
 			bullet.draw_bullet()
+		self.aliens.draw(self.screen)
+		
 		pygame.display.flip()
 
-
+	def _create_fleet(self):
+		alien = Alien(self)
+		self.aliens.add(alien)
 
 
 if __name__ == '__main__':
